ca1b6ddb15892c3f41f5587c11eb5cc41a19a058
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-04-23 12:08:00 +0800

    Update index.js
    Signed-off-by: Waren Gador <123795328+Renstrio24p@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Page from "./src/react"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Page />)
+const DOM = ReactDOM.createRoot(document.getElementById('root'))
+DOM.render(<Page />)
                                                             
 // You can start coding right here with your experiment
+
+/*
+  Note: React 17 vs React 18
+
+  ReactDOM.render(<Page />, document.getElementById('root')) << React 17
+  
+  vs
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(<Page /> << React 18
+
+  or 
+  
+  const DOM = ReactDOM.createRoot(document.getElementById('root')) << React 18 with const it in a variable
+  DOM.render(<Page />
+
+*/
