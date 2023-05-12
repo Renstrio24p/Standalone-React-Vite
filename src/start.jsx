1256797cdff904edb59ab2c0854f9cc1c6af3dea
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-12 23:33:05 +0800

    Update start.jsx
    Signed-off-by: Waren Gador <123795328+Renstrio24p@users.noreply.github.com>

--- src/start.jsx
+++ src/start.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export default function Start = (
+export default function Start() {
+    
     <div>
         <h1>Hello React</h1>
         {/* You can start coding right here..*/}
     </div>
-)
+}
