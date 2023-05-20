d40f91f9b5f744ea7fb9410baeee3c003833cfb2
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-21 06:25:08 +0800

    add all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "https://kit.fontawesome.com/6b203b1712.js";
 import "./index.css";
 import ReactApp from "./react-folder/react";
 
+function Page () {
+    return (
+        ReactApp()
+    )
+}
 
 
 const DOM = ReactDOM.createRoot(document.getElementById("root"));
-DOM.render(<ReactApp />);
+DOM.render(Page());
