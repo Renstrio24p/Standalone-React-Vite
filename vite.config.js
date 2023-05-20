05dc0cc9cb19870ecfd81335e34d401a443c6d97
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-21 07:42:15 +0800

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : './',
+  base : '/https://github.com/Renstrio24p/Standalone-React-Vite',
 })
