c128f641f523db7ef1d3b51ad29a95f79da1be52
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-21 06:38:51 +0800

    add all

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
-import ReactDOM from "./react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "https://kit.fontawesome.com/6b203b1712.js";
 import "./index.css";
 import ReactApp from "./react-folder/react";
 
 function Page () {
     return (
-        ReactApp()
+        <React.StrictMode>
+            <ReactApp />
+        </React.StrictMode>
     )
 }
 
 
 const DOM = ReactDOM.createRoot(document.getElementById("root"));
-DOM.render(Page());
+DOM.render(<Page />);
