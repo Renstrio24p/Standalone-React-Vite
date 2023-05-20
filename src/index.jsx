0af2e16c6c0b5894218be0333da7433aff1c77fb
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-21 02:56:37 +0800

    add all

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "https://kit.fontawesome.com/6b203b1712.js";
 import "./index.css";
 import ReactApp from "./react-folder/react";
-import Start from "./start";
 
-const Page = (
-  <React.StrictMode>
-    <BrowserRouter>
-      <ReactApp />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
 
 const DOM = ReactDOM.createRoot(document.getElementById("root"));
-DOM.render(Page);
+DOM.render(<ReactApp />);
