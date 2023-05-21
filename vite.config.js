2f04b4e6489b5842f456d360ee20df6755f526c8
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-21 08:04:47 +0800

    reconstruct workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/https://github.com/Renstrio24p/Standalone-React-Vite',
 })
