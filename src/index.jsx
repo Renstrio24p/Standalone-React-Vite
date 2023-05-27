e636a42e77c0d03d1c79974e3eb47451e3bd424e
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-28 05:01:20 +0800

    add .

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Start from './start.jsx'
+import ReactApp from './react-18/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ReactApp />
+  </React.StrictMode>
+)
---
+++ src/start.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Start(){
+    return (
+        <div>
+            <h1>Hello React </h1>
+            {/* You can start coding right here */}
+        </div>
+    )
+}
+
+// This is the same as APP.JS
